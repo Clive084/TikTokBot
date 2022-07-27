2c894cfba2a0c70280eaf1e7a2b98d18d9cb7369
Clive084 <110111248+Clive084@users.noreply.github.com>
2022-07-27 16:11:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -20,9 +20,14 @@ while True:
     try:
         driver.find_element(By.XPATH,'//*[@id="wrapper"]/div[1]/main/app-temporary-phone-number-generator/section/div/div[1]/div/div/div[1]/button').click()
         break
+    except Exception as e:
+        print(e)
+while True:
+    try:
+        driver.find_element(By.XPATH,'//*[@id="wrapper"]/div[1]/main/app-temporary-phone-number-generator/section/div/div[1]/div/div/div[2]/div/a[24]').click()
+        break
     except:
         pass
-driver.find_element(By.XPATH,'//*[@id="wrapper"]/div[1]/main/app-temporary-phone-number-generator/section/div/div[1]/div/div/div[2]/div/a[24]').click()
 driver.find_element(By.XPATH,'//*[@id="wrapper"]/div[1]/main/app-temporary-phone-number-generator/section/div/div[1]/div/div/button').click()
 while True:
     try:
@@ -30,8 +35,7 @@ while True:
         break
     except:
         pass
-
-
+phone_number=driver.find_element(By.XPATH,'//*[@id="wrapper"]/div[1]/main/messages/section/div/div/div/h1').text
 driver.execute_script("window.open('');")
 driver.switch_to.window(driver.window_handles[1])
 driver.get("https://www.tiktok.com/signup/phone-or-email/phone")
@@ -49,8 +53,22 @@ driver.find_element(By.XPATH,'//*[@id="loginContainer"]/div[1]/form/div[3]/div[3
 driver.find_element(By.XPATH,'//*[@id="loginContainer"]/div[1]/form/div[6]/div/div[1]/div').click()
 driver.find_element(By.XPATH,'//*[@id="loginContainer"]/div[1]/form/div[6]/div/div[1]/div[2]/div[1]/input').send_keys("uk")
 driver.find_element(By.XPATH,'//*[@id="U"]/li[2]/span').click()
-driver.find_element(By.XPATH,'//*[@id="loginContainer"]/div[1]/form/div[6]/div/div[2]/input').send_keys(Keys.CONTROL, 'v')
+driver.find_element(By.XPATH,'//*[@id="loginContainer"]/div[1]/form/div[6]/div/div[2]/input').send_keys(phone_number[3:])
 driver.find_element(By.XPATH,'//*[@id="loginContainer"]/div[1]/form/div[7]/div/button').click()
+time.sleep(3)
+try:
+    if driver.find_element(By.XPATH,'//*[@id="loginContainer"]/div[1]/form/div[6]/div[2]'):
+        print("dfdwadafwagawgawggsdfghjljkhgghhgfhgfgfdsfsdfadswqewqrewtretreyreutrut")
+except:
+    try:
+        if driver.find_element(By.XPATH,'//*[@id="captcha_container"]/div/div[1]/div[2]/div'):
+            while True:
+                try:
+                    driver.find_element(By.XPATH,'//*[@id="captcha_container"]/div/div[1]/div[2]/div')
+                except:
+                    break
+    except:
+        pass
 driver.switch_to.window(driver.window_handles[0])
 
 input()
